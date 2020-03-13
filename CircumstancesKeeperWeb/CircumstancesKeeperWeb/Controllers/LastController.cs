58abe1234ce58d926735407c9cdb192fcf8a3ccc
M.Tonosaki <56340279+tmctono@users.noreply.github.com>
2020-03-13 17:47:21 +0900

    fix the issue #12, #13 (#14)
    * Slider Support
    
    * touch support
    
    * Fix Issue #4@mtonosaki
    
    * FIx the issue #6@mtonosaki
    
    * Fix the issue #5@mtonosaki
    
    * Support responsive design : Issue #10@mtonosaki
    
    * Fix the issue #12@mtonosaki
    
    * Issue #12, #13@mtonosaki
    
    Co-authored-by: Manabu Tonosaki <manabu@tomarika.com>
    Co-authored-by: Manabu Tonosaki <34669114+mtonosaki@users.noreply.github.com>

---
+++ CircumstancesKeeperWeb/CircumstancesKeeperWeb/Controllers/LastController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Tono;
+
+namespace CircumstancesKeeperWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LastController : ControllerBase
+    {
+        private readonly ILogger<LastController> _logger;
+
+        public LastController(IOptions<MyConfig> config, ILogger<LastController> logger)
+        {
+            Common.Prepare(config);
+            _logger = logger;
+        }
+
+        // GET: api/Frame
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return StatusCode(400); // Bad Request
+        }
+
+        // GET: api/Last/location0001    https://localhost:44310/api/Last/location0001
+        /// <summary>
+        /// Get the latest camera image (JPEG)
+        /// </summary>
+        /// <param name="l">Location code</param>
+        /// <returns>Last item DateTime</returns>
+        [HttpGet("{l}", Name = "GetLast")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<ActionResult<DateTime>> Get(string l)
+        {
+            var utc = DateTime.UtcNow + TimeSpan.FromSeconds(60);
+            var conname = $"cam-{utc.ToString("yyyyMMdd")}-{l}";
+            var token = default(BlobContinuationToken);
+            var blobcontainer = Common.Blobs.GetValueOrDefault(conname, true, a => Common.Blob.GetContainerReference(conname));
+            for (var i = 0; i < 5; i++)
+            {
+                var sret = await blobcontainer.ListBlobsSegmentedAsync($"{utc.ToString("yyyyMMdd")}-{utc.ToString("HHmm")}", true, BlobListingDetails.Metadata, null, token, null, null);
+                var col =
+                    from r in sret.Results
+                    let cbb = r as CloudBlockBlob
+                    where cbb != null
+                    orderby cbb.Name descending
+                    select cbb;
+                var first = col.FirstOrDefault();
+                if (first != default)
+                {
+                    var dt = DateTime.Parse(first.Metadata["EventTime"]);
+                    return dt;
+                } else
+                {
+                    utc -= TimeSpan.FromMinutes(1.0);
+                }
+            }
+            return StatusCode(404);
+        }
+    }
+}
--- CircumstancesKeeperWeb/CircumstancesKeeperWeb/Startup.cs
+++ CircumstancesKeeperWeb/CircumstancesKeeperWeb/Startup.cs
@@ -59,9 +59,7 @@ namespace CircumstancesKeeperWeb
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute( name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
